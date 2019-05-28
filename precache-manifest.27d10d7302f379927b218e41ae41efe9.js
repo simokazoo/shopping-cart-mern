253ef6d7add3ce7243173d5c6bb197f61e02dcce
simokazoo <french.simona@yahoo.com>
2019-05-28 14:26:16 +0200

    Updates

---
+++ precache-manifest.27d10d7302f379927b218e41ae41efe9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da937d172e2f3eef21b7725a1a6e7ba",
+    "url": "/simokazoo/shopping-cart-mern.git/index.html"
+  },
+  {
+    "revision": "fbfada8889f7e5d2cc52",
+    "url": "/simokazoo/shopping-cart-mern.git/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "fe5f4ddab4543e979c47",
+    "url": "/simokazoo/shopping-cart-mern.git/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fbfada8889f7e5d2cc52",
+    "url": "/simokazoo/shopping-cart-mern.git/static/js/2.35ff6705.chunk.js"
+  },
+  {
+    "revision": "fe5f4ddab4543e979c47",
+    "url": "/simokazoo/shopping-cart-mern.git/static/js/main.53779d23.chunk.js"
+  },
+  {
+    "revision": "d1662fa7dc92dc9c5fd3",
+    "url": "/simokazoo/shopping-cart-mern.git/static/js/runtime~main.8b39f79b.js"
+  }
+]);
